428f8ee0fc4ff880db932d6064ccab392de5832f
Md. Almas Ali <almaspr3@gmail.com>
2022-08-22 01:28:41 +0600

    Moved all files on main root directory.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi:app
+web: gunicorn passenger_wsgi:app
--- app/passenger_wsgi.py
+++
@@ -1,7 +0,0 @@
-import sys, os
-
-INTERP = "<PATH TO PYTHON3 VIRTUAL ENV>/bin/python"
-
-if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
-
-from main import app as application
