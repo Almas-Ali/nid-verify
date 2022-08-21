38d4cd3399ab54ac29b5bcef8e66ff13f11c59f2
Md. Almas Ali <almaspr3@gmail.com>
2022-08-22 01:44:11 +0600

    Fixed db error

--- database/models.py
+++ database/models.py
@@ -1,6 +1,6 @@
 from peewee import *
 from datetime import datetime as dt
-
+import psycopg2
 
 # db = SqliteDatabase('nid.db')
 db = PostgresqlDatabase('df4u2hod95hlv0', user='xxzhjdzfhkzcff', password='e0ccef1e059f71975c415a7a5ec8031d074c3ffe6f3d0f6d3f8bdd52b34fbeab', host='ec2-44-206-137-96.compute-1.amazonaws.com', port=5432)
